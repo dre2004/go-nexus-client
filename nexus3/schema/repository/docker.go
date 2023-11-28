1f8d66a7fa5413def7942be7a4e4b45037e35574
dre2004 <dre2004@gmail.com>
2023-11-28 22:11:19 +1100

    update subdomain to be a string

--- nexus3/schema/repository/docker.go
+++ nexus3/schema/repository/docker.go
@@ -57,7 +57,7 @@ type Docker struct {
 	// Whether to allow clients to use the V1 API to interact with this repository
 	V1Enabled bool `json:"v1Enabled"`
 	// Use sub-domain routing for this repository
-	SubDomain bool `json:"subdomain"`
+	SubDomain *string `json:"subdomain"`
 }
 
 // DockerProxy contains data of a Docker Proxy Repository
